9307c84384fc19baea777af55dc397e754809ceb
alejandrox1 <alarcj137@gmail.com>
2017-11-21 14:51:32 -0500

    add comments and rm unimported packages from parse_sql

--- parse_sql.go
+++ parse_sql.go
@@ -1,15 +1,16 @@
 package parse_sql
 
 import (
-	"fmt"
 	"io/ioutil"
 	"strings"
 )
 
+// Remove any extrawhitespaces, tabs, and newlines from the command.
 func cleanString(s string) string {
 	return strings.Join(strings.Fields(s), " ")
 }
 
+// Return a slice of strings with nonempty strings.
 func delEmptyLines(s []string) []string {
 	var newSlice []string
 	for _, str := range s {
@@ -20,6 +21,8 @@ func delEmptyLines(s []string) []string {
 	return newSlice
 }
 
+// Take sql commands as a single string and return a slice of strings where
+// each string is an sql command.
 func trimSQLCmds(s string) []string {
 	cmds := strings.SplitAfter(s, ";")
 	for i, c := range cmds {
@@ -28,19 +31,13 @@ func trimSQLCmds(s string) []string {
 	return delEmptyLines(cmds)
 }
 
-func sqlCmds(filename string) []string {
+// Given a filename containing all sql commands to be executed, return a slice
+// of strings with all the commands parsed.
+func SQLCmds(filename string) []string {
 	sqlSetup, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
 
 	return trimSQLCmds(string(sqlSetup))
-}
-
-func main() {
-
-	sqlCommands := sqlCmds("setup.sql")
-	for i, cmd := range sqlCommands {
-		fmt.Printf("%d) \"%s\"\n", i, cmd)
-	}
 }
